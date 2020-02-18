e43f529be37b90978094bf0a02520011709a08e7
YtaloWill <ytalowillgloria@gmail.com>
2020-02-18 00:43:19 -0300

    Fully functional

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [binary, setBinary] = useState()
+	const [decimal, setDecimal] = useState()
+
+	function updateDecimal(event) {
+		setBinary(event.target.value)
+		var equivalentDecimal = 1
+		var result = 0
+
+		for(let i = event.target.value.length-1; i >= 0; i--) {
+			result += parseInt(event.target.value[i]) * equivalentDecimal
+			equivalentDecimal *= 2
+		}
+		setDecimal(result)
+	}
+
+	function updateBinary(event) {
+		setDecimal(event.target.event)
+		var total = parseInt(event.target.value)
+		var reminder = ''
+		while (total > 0) {
+			reminder = reminder + total % 2
+			total = ~~(total / 2)
+		}
+		setBinary(reminder.split('').reverse().join(''))
+	}
+
+ 	return (
+		<div className="App">
+			<h1>Binary to Decimal Conversor</h1>
+			 
+			 <label>Binary Number</label>
+			 <input 
+			   className="binary" 
+			   onChange={event => updateDecimal(event)}
+			   value={binary}
+			 />
+			 
+			 <label>Decimal Number</label>
+			 <input 
+			   className="decimal" 
+			   type="number"
+			   onChange={event => updateBinary(event)}
+			   value={decimal}
+			 />
+
+		</div>
+	);
 }
 
 export default App;
