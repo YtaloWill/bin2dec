2eb4d18ab24f932a442f9dde307c65e3d70c66d5
YtaloWill <ytalowillgloria@gmail.com>
2020-02-24 17:38:26 -0300

    Binary restriction added

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Binary to Decimal numbers converter
 
-Project idea from [this github repository](https://github.com/florinpop17/app-ideas) but with with adaptations
+Project idea from [this github repository](https://github.com/florinpop17/app-ideas) but with adaptations
 
 ### This project contains:
 
 - [x] Block characters non-numbers in decimal field
-- [ ] Allow only 0-1 in binary field
+- [x] Allow only 0-1 in binary field
 - [x] Realtime converter
 - [x] User can convert decimal to binary
--- src/App.js
+++ src/App.js
@@ -8,20 +8,37 @@ function App() {
 	const [decimal, setDecimal] = useState()
 
 	function updateDecimal(event) {
-		setBinary(event.target.value)
+		if (event === ""){
+			setBinary(event)
+			setDecimal(event)
+			return
+		}
+
+		const regex = /^[0-1]{1,}$/g
+		if (!regex.test(parseInt(event))){
+			alert("Only 0 or 1 please :3")
+			return
+		}
+		setBinary(event)
 		var equivalentDecimal = 1
 		var result = 0
 
-		for(let i = event.target.value.length-1; i >= 0; i--) {
-			result += parseInt(event.target.value[i]) * equivalentDecimal
+		for(let i = event.length-1; i >= 0; i--) {
+			result += parseInt(event[i]) * equivalentDecimal
 			equivalentDecimal *= 2
 		}
 		setDecimal(result)
 	}
 
 	function updateBinary(event) {
-		setDecimal(event.target.event)
-		var total = parseInt(event.target.value)
+		if (event === ""){
+			setDecimal(event)
+			setBinary(event)
+			return
+		}
+
+		setDecimal(event)
+		var total = parseInt(event)
 		var reminder = ''
 		while (total > 0) {
 			reminder = reminder + total % 2
@@ -36,8 +53,9 @@ function App() {
 			<div className="container">
 				<label>Binary Number</label>
 				<input 
-					className="binary" 
-					onChange={event => updateDecimal(event)}
+					className="binary"
+					type="number"
+					onChange={event => updateDecimal(event.target.value)}
 					value={binary}
 				/>
 				
@@ -45,7 +63,7 @@ function App() {
 				<input 
 					className="decimal" 
 					type="number"
-					onChange={event => updateBinary(event)}
+					onChange={event => updateBinary(event.target.value)}
 					value={decimal}
 				/>
 				<p><i>Tip: </i>Try convert decimal to binary too! ;)</p>
